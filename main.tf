27cd5bee9f1f40f608932775acb73830345fa114
Jacob Martinson <jacobm3@gmail.com>
2019-08-01 22:26:05 -0500

    remove entire label blockfrom main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,6 @@ resource "google_compute_instance" "instance" {
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
-  labels = {
-    env   = "default"
-  }
-
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
